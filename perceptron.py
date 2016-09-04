d565066474aa56b518285ed9dd142eed7601e6e3
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-04 15:54:52 +0530

    Initial Perceptron push

---
+++ perceptron.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Perceptron:
+    def __init__(self,x,y):
+        self.ntrain = x.shape[0]
+        self.dim = x.shape[1]
+        self.x = x
+        self.y = y
+        self.w = np.random.random((dim,1))
+
+    def plotBoundary():
+
+
+    def train(self):
+        pass
