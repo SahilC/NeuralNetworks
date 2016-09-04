91be20170d319ece030257ff90d1cae23f8e3ef9
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-04 19:26:52 +0530

    Added function to process input

--- perceptron.py
+++ perceptron.py
@@ -19,11 +19,14 @@ class Perceptron:
         for i in xrange(len(self.x)):
             #print str(self.x[i][0])+ ' ' + str(self.x[i][1])
             plt.scatter(self.x[i][0],self.x[i][1],marker=vals[self.y[i]])
-        x = np.linspace(-10, 10, 10)
-        y = -1*(self.w[0]/self.w[1])*x - (1/self.w[1])
+        x = np.linspace(-10, 20, 10)
+        y = -1*(self.w[0]/self.w[1])*x - (self.w[2]/self.w[1])
         #print y
-        plt.plot(x,y,c='r')
+        plt.plot(x,y,'r--')
         plt.show()
+
+    def processInput(self):
+        self.x[self.y < 0] = -1*self.x[self.y < 0]
 
     def train(self):
         # out =  np.abs(np.sign(np.sum(self.w.transpose()*self.x,axis=1)) - self.y)
@@ -43,6 +46,7 @@ class Perceptron:
         #     out =  np.abs(np.sign(np.sum(self.w.transpose()*self.x,axis=1)) - self.y)
         #     val = np.sum(out)
         # self.plotBoundary()
+        self.processInput()
         e = 1
         k = 0
         while e > 0:
@@ -55,11 +59,11 @@ class Perceptron:
                     self.w[1] += self.x[i,1]
                     self.w[2] += self.x[i,2]
                     e += 1
-        print k
+        self.processInput()
         self.plotBoundary()
 
 if __name__ == '__main__':
-    x = np.array([[2, 7, 1], [8, 1, 1], [7, 5, 1], [6, 3, 1],[7, 8, 1],[5, 9, 1],[4, 5,1],[-4, -2, -1],[1, 1,-1],[-1, -3,-1], [-3, 2,-1], [-5, -3.25,-1], [-2, -4,-1],[-7, -1, -1]])
+    x = np.array([[2, 7, 1], [8, 1, 1], [7, 5, 1], [6, 3, 1],[7, 8, 1],[5, 9, 1],[4, 5,1],[4, 2, 1],[-1, -1,1],[1, 3,1], [3, -2,1], [5, 3.25,1], [2, 4,1],[7, 1, 1]])
     y = np.array([1, 1, 1, 1, 1, 1, 1,-1, -1, -1, -1, -1, -1,-1])
     p = Perceptron(x,y)
     #p.plotBoundary()
