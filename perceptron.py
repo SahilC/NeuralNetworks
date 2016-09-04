c4583c7badbd9e411b50595af33969eb6361dfcd
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-04 16:21:46 +0530

    Initial Visualization function

--- perceptron.py
+++ perceptron.py
@@ -7,10 +7,22 @@ class Perceptron:
         self.dim = x.shape[1]
         self.x = x
         self.y = y
-        self.w = np.random.random((dim,1))
+        self.w = np.random.random((self.dim,1))
 
-    def plotBoundary():
-
+    def plotBoundary(self):
+        a = self.x[:,0]
+        b = self.x[:,1]
+        vals = {1:'+',-1:'<'}
+        for i in xrange(len(x)):
+            plt.scatter(self.x[i][0],self.x[i][1],marker=vals[self.y[i]])
+        plt.show()
 
     def train(self):
         pass
+
+
+if __name__ == '__main__':
+    x = np.array([[2, 7], [8, 1], [7, 5], [6, 3],[7, 8],[5, 9],[4, 5],[4, 2],[-1, -1],[1, 3], [3, -2], [5, 3.25], [2, 4],[7, 1]])
+    y = np.array([1, 1, 1, 1, 1, 1, 1,-1, -1, -1, -1, -1, -1,-1])
+    p = Perceptron(x,y)
+    p.plotBoundary()
