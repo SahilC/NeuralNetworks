62ec39c2af1ee3d436179cde27adb66754be8e9c
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-12 23:23:16 +0530

    Code fixes

--- neuron.py
+++ neuron.py
@@ -19,23 +19,42 @@ class Neuron:
         return np.sum(self.w*x)
 
     def out_value(self,x):
+        # print self.w
+        # print x
         self.y = sigmoid(self.sum_value(x))
+        # print self.y
+        # print '========'
         return self.y
 
     def update_weight_hidden(self,x,t):
         delta_k = (self.y - t)*self.y*(1 - self.y)
         for i in xrange(len(self.w)):
-            self.w[i] = self.w[i] - self.learning_rate*delta_k*x[i]
+            self.w[i] = self.w[i] + self.learning_rate*delta_k*x[i]
+
+        # print 'UPDATED_HIDDEN'
+        # print self.y
+        # print t
+        # print delta_k
+        # print self.w
+        # print 'xxxxxxxxxxxxxxxx'
         return delta_k
 
     def calculate_error(self,t):
         return (0.5*(self.y - t)**2)
 
-    def update_weight_input(self,x,neuron_hidden, neuron_out,delta_k):
+    def update_weight_input(self,x,neuron_hidden,hidden_idx, neuron_out,delta_k):
         val = 0
-        for j in xrange(len(neuron_hidden)):
-            for n in xrange(len(neuron_out)):
-                val += delta_k[n]*neuron_out[n].w[j]
+        for n in xrange(len(neuron_out)):
+            val += delta_k[n]*neuron_out[n].w[hidden_idx]
+            # print delta_k[n]
+            # print neuron_out[n].w[hidden_idx]
+            # print '-----------------------'
+
+        # print 'UPDATE_OUT'
+        # print val
 
         for i in xrange(len(self.w)):
             self.w[i] = self.w[i] - self.learning_rate*val*self.y*(1-self.y)*x[i]
+
+        # print self.w
+        # print 'yyyyyyyyyyyyyyyyyyyyyy'
