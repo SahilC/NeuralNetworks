c57a1d35d1e716f38e0ce749d66eef6c31945768
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-09-14 11:12:12 +0530

    Fixes for LMS

--- perceptron.py
+++ perceptron.py
@@ -9,8 +9,8 @@ class Perceptron:
         self.y = y
         #self.w = np.random.random((self.dim,1))
         #print self.w.shape
-        self.w = np.random.random((self.dim))
-        #self.w = np.array([1.0,1.0,1.0])
+        #self.w = np.random.random((self.dim))
+        self.w = np.array([1.0,1.0,1.0])
 
     def plotBoundary(self):
         a = self.x[:,0]
@@ -19,7 +19,7 @@ class Perceptron:
         for i in xrange(len(self.x)):
             #print str(self.x[i][0])+ ' ' + str(self.x[i][1])
             plt.scatter(self.x[i][0],self.x[i][1],marker=vals[self.y[i]])
-        x = np.linspace(-10, 20, 10)
+        x = np.linspace(0, 10, 10)
         y = -1*(self.w[0]/self.w[1])*x - (self.w[2]/self.w[1])
         #print y
         plt.plot(x,y,'r--')
@@ -60,7 +60,7 @@ class Perceptron:
                     self.w[1] += a*self.x[i,1]
                     self.w[2] += a*self.x[i,2]
                     e += 1
-        print k
+        #print k
         self.processInput()
         self.plotBoundary()
 
@@ -92,23 +92,25 @@ class Perceptron:
         self.processInput()
         e = 1
         k = 0
-        while True:
+        eta = a
+        while k < 1000:
             k += 1
-            e = 0
             for i in xrange(len(self.x)):
                 val = np.sum(self.w*self.x[i])
-                #self.plotBoundary()
-                update = (b - val)
-                self.w[0] += a*update*self.x[i,0]
-                self.w[1] += a*update*self.x[i,1]
-                self.w[2] += a*update*self.x[i,2]
-                if np.sum((a*update*self.x[i])**2) < 0.001:
-                    self.processInput()
-                    self.plotBoundary()
-                    return
+                if ((b - val) < 0.5):
+                    update = 0
+                else:
+                    update = 1
+                self.w += eta*update*self.x[i]
+        self.processInput()
+        print self.w
+        self.plotBoundary()
+        return
+
 if __name__ == '__main__':
-    # y = np.array([1, 1, 1, 1, 1, 1, 1,-1, -1, -1, -1, -1, -1,-1])
-    # p = Perceptron(x,y)
-    # #p.plotBoundary()
-    # #p.trainRelaxation(0.005,1)
-    # p.plotBoundary()
+    x = np.array([[2, 7, 1], [8, 1, 1], [7, 5, 1], [6, 3, 1],[7, 8, 1],[5, 9, 1],[4, 5,1],[4, 2, 1],[-1, -1,1],[1, 3,1], [3, -2,1], [5, 3.25,1], [2, 4,1],[7, 1, 1]])
+    y = np.array([1, 1, 1, 1, 1, 1, 1,-1, -1, -1, -1, -1, -1,-1])
+    p = Perceptron(x,y)
+    #p.plotBoundary()
+    #p.trainRelaxation(0.005,1)
+    p.trainLMS(0.1,1)
